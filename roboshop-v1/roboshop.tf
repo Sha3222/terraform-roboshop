e50117829f0711c8600e62d74fe2a4e53614df26
Bablu <bablu.chilukoti@gmail.com>
2024-01-25 01:49:25 +0530

    Added all the required instances

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -1,8 +1,80 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "frontend" {
   ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+resource "aws_instance" "mongodb" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+resource "aws_instance" "catalogue" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+resource "aws_instance" "redis" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "redis"
+  }
+}
+resource "aws_instance" "user" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "user"
+  }
+}
+resource "aws_instance" "cart" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "cart"
+  }
+}
+resource "aws_instance" "mysql" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+resource "aws_instance" "shipping" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "shipping"
+  }
+}
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
   tags = {
-    Name = "first-ec2"
+    Name = "rabbitmq"
+  }
+}
+resource "aws_instance" "payment" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "payment"
   }
 }
