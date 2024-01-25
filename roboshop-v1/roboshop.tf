4d25ed27009bdc2612c534f461e3f775b01faae1
Bablu <bablu.chilukoti@gmail.com>
2024-01-25 23:53:06 +0530

    route53

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -24,6 +24,14 @@ resource "aws_instance" "mongodb" {
     Name = "mongodb"
   }
 }
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z082591214E2K79TAHGZM"
+  name    = "mongodb-dev.sreddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
 resource "aws_instance" "catalogue" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
@@ -32,6 +40,14 @@ resource "aws_instance" "catalogue" {
   tags = {
     Name = "catalogue"
   }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z082591214E2K79TAHGZM"
+  name    = "catalogue-dev.sreddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
 }
 resource "aws_instance" "redis" {
   ami           = "ami-0f3c7d07486cad139"
@@ -42,6 +58,13 @@ resource "aws_instance" "redis" {
     Name = "redis"
   }
 }
+resource "aws_route53_record" "redis" {
+  zone_id = "Z082591214E2K79TAHGZM"
+  name    = "redis-dev.sreddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
 resource "aws_instance" "user" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
@@ -50,6 +73,13 @@ resource "aws_instance" "user" {
   tags = {
     Name = "user"
   }
+}
+resource "aws_route53_record" "redis" {
+  zone_id = "Z082591214E2K79TAHGZM"
+  name    = "redis-dev.sreddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
 resource "aws_instance" "cart" {
   ami           = "ami-0f3c7d07486cad139"
@@ -60,6 +90,13 @@ resource "aws_instance" "cart" {
     Name = "cart"
   }
 }
+resource "aws_route53_record" "cart" {
+  zone_id = "Z082591214E2K79TAHGZM"
+  name    = "cart-dev.sreddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
 resource "aws_instance" "mysql" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
@@ -68,6 +105,13 @@ resource "aws_instance" "mysql" {
   tags = {
     Name = "mysql"
   }
+}
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z082591214E2K79TAHGZM"
+  name    = "mysql-dev.sreddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
 resource "aws_instance" "shipping" {
   ami           = "ami-0f3c7d07486cad139"
@@ -78,6 +122,13 @@ resource "aws_instance" "shipping" {
     Name = "shipping"
   }
 }
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z082591214E2K79TAHGZM"
+  name    = "shipping-dev.sreddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
 resource "aws_instance" "rabbitmq" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
@@ -87,6 +138,13 @@ resource "aws_instance" "rabbitmq" {
     Name = "rabbitmq"
   }
 }
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z082591214E2K79TAHGZM"
+  name    = "shipping-dev.sreddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
 resource "aws_instance" "payment" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
@@ -95,4 +153,11 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+resource "aws_route53_record" "payment" {
+  zone_id = "Z082591214E2K79TAHGZM"
+  name    = "payment-dev.sreddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }
