08af164c7f7988a942cbd8dffc81d5bd716e72fc
Bablu <bablu.chilukoti@gmail.com>
2024-01-27 03:09:46 +0530

    variables

--- sample_variables.tf
+++ sample_variables.tf
@@ -24,7 +24,7 @@ output "fruit_name" {
 variable "fruits" {
 default = ["Apple" , "Orange" , "Mango"]
 }
-
+## Accessing the list of variables
 output "fruits_names" {
  value = var.fruits[0]
 
@@ -38,4 +38,35 @@ output "fruits_names1" {
 output "fruits_names2" {
  value = var.fruits[2]
 
+}
+
+##Map of variables
+
+variable "fruit_stock" {
+default = {
+  apple = 100
+  banana = 200
+}
+}
+
+variable "fruits_price_stock" {
+ default = {
+  apple =  {
+   stock = 100
+   price = 3
+ }
+ banana = {
+  stock = 400
+  price = 1
+ }
+ }
+}
+
+## Accessing the map of variables
+output "fruits_stock_apple" {
+ value = var.fruit_stock[apple]
+}
+
+output "fruits_price_stock_banana" {
+ value = var.fruit_stock[banana]
 }
