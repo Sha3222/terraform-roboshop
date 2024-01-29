627f5bdc013b38e6ba38cbda8632b8486f6b2a71
Bablu <bablu.chilukoti@gmail.com>
2024-01-29 16:22:47 +0530

    Added functions and loops/count & for each

--- roboshop-v2/roboshop-v2.tf
+++ roboshop-v2/roboshop-v2.tf
@@ -47,5 +47,5 @@ resource "aws_route53_record" "DNS-records" {
  name    = "${lookup(each.value, "name", null)}.sreddy.online"
  type    = "A"
  ttl     = 30
- records = [lookkup(lookup(aws_instance.instances, each.key, null), "private_ip", null) ]
+ records = [lookup(lookup(aws_instance.instances, each.key, null), "private_ip", null) ]
 }
