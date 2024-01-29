abd1aa1b4c6b77638259dc4123ead390023f8dda
Bablu <bablu.chilukoti@gmail.com>
2024-01-29 15:26:40 +0530

    Added functions and loops/count & for each

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # terraform-roboshop
+
+Never user list as input in terraform always use map.
+Be careful while using count.
+
--- roboshop-v2/roboshop-v2.tf
+++ roboshop-v2/roboshop-v2.tf
@@ -14,183 +14,38 @@ variable "zone" {
 default = "Z082591214E2K79TAHGZM"
 }
 
+variable "components" {
+ default = {
+ frontend = { name = "frontend-dev" }
+ catalogue = { name = "catalogue-dev" }
+ mongodb = { name = "mongodb-dev" }
+ user = { name = "user-dev" }
+ redis = { name = "redis-dev" }
+ cart = { name = "cart-dev" }
+ mysql = { name = "mysql-dev" }
+ shipping = { name = "shipping-dev" }
+ payment = { name = "payment-dev" }
+ rabbitmq = { name = "rabbitmq-dev" }
 
-resource "aws_instance" "frontend" {
+ }
+}
+
+resource "aws_instance" "instances" {
+
+  for_each = var.components
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = var.security_group
 
   tags = {
-    Name = "frontend"
+    Name = lookup(each.key, each.value["name"], "null")
   }
 }
 resource "aws_route53_record" "frontend" {
+  for_each = var.components
   zone_id = var.zone
-  name    = "frontend-dev.sreddy.online"
+  name    = "${lookup(each.value, each.key["name", null])}.sreddy.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
-
-resource "aws_instance" "mongodb" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-resource "aws_route53_record" "mongodb" {
-  zone_id = var.zone
-  name    = "mongodb-dev.sreddy.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
-resource "aws_instance" "catalogue" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-
-resource "aws_route53_record" "catalogue" {
-  zone_id = var.zone
-  name    = "catalogue-dev.sreddy.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
-resource "aws_instance" "redis" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group
-
-  tags = {
-    Name = "redis"
-  }
-}
-resource "aws_route53_record" "redis" {
-  zone_id = var.zone
-  name    = "redis-dev.sreddy.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.redis.private_ip]
-}
-resource "aws_instance" "user" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group
-
-  tags = {
-    Name = "user"
-  }
-}
-resource "aws_route53_record" "user" {
-  zone_id = var.zone
-  name    = "user-dev.sreddy.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.user.private_ip]
-}
-resource "aws_instance" "cart" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group
-
-  tags = {
-    Name = "cart"
-  }
-}
-resource "aws_route53_record" "cart" {
-  zone_id = var.zone
-  name    = "cart-dev.sreddy.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.cart.private_ip]
-}
-resource "aws_instance" "mysql" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group
-
-  tags = {
-    Name = "mysql"
-  }
-}
-resource "aws_route53_record" "mysql" {
-  zone_id = var.zone
-  name    = "mysql-dev.sreddy.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
-resource "aws_instance" "shipping" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group
-
-  tags = {
-    Name = "shipping"
-  }
-}
-resource "aws_route53_record" "shipping" {
-  zone_id = var.zone
-  name    = "shipping-dev.sreddy.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.shipping.private_ip]
-}
-resource "aws_instance" "rabbitmq" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = var.zone
-  name    = "rabbitmq-dev.sreddy.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.rabbitmq.private_ip]
-}
-resource "aws_instance" "payment" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group
-
-  tags = {
-    Name = "payment"
-  }
-}
-resource "aws_route53_record" "payment" {
-  zone_id = var.zone
-  name    = "payment-dev.sreddy.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.payment.private_ip]
-}
-
-resource "aws_instance" "dispatch" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group
-  tags = {
-    Name = "dispatch"
-  }
-}
-resource "aws_route53_record" "dispatch" {
-  zone_id = var.zone
-  name    = "dispatch-dev.sreddy.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.dispatch.private_ip]
+  //records = [aws_instance.frontend.private_ip]
 }
