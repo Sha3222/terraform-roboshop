da1464e7134309587699fbcc309587fcb9728192
Bablu <bablu.chilukoti@gmail.com>
2024-02-07 14:25:53 +0530

    for-each

--- 2.loops/count/for-each/main.tf
+++ 2.loops/count/for-each/main.tf
@@ -20,7 +20,7 @@ variable "security-groups" {
 resource "aws_instance" "instance" {
   for_each               =  var.components
   ami                    =  var.ami
-  instance_type          =  var.instance_type
+  instance_type          =  var.instance-type
   vpc_security_group_ids =  var.security-groups
 
   tags = {
