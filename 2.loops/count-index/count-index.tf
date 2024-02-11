1b6120725d413017012c87f72e170a90a58ce4b4
Bablu <bablu.chilukoti@gmail.com>
2024-02-11 19:22:33 +0530

    count-index

--- 2.loops/count-index/count-index.tf
+++ 2.loops/count-index/count-index.tf
@@ -1,5 +1,5 @@
 variable "ec2" {
-default = ["ec21", "ec2"]
+default = [ "ec1", "ec2" ]
 }
 variable "ami" {
 default = "ami-0f3c7d07486cad139"
@@ -13,11 +13,12 @@ default = "t2.micro"
 
 
 resource "aws_instance" "instances" {
+count = var.ec2
 ami           = var.ami
 instance_type = var.instance-type
 vpc_security_group_ids = var.security-id
 
 tags = {
- Name = var.ec2.${count.index}
+ Name = count.index
 }
 }
