004986f71e6b2dc43a082741696d7db0eec069a6
Bablu <bablu.chilukoti@gmail.com>
2024-02-21 01:18:18 +0530

    code

--- 3.module/main.tf
+++ 3.module/main.tf
@@ -1,5 +1,5 @@
 module "ec2-machines" {
-source = ../local-module
+source = "../local-module"
 ami           = var.ami
 instance_type = var.instance-type
 vpc_security_group_ids = var.security-group
