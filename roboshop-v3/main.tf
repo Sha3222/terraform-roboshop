3478009dc9e019ff9bd1704ac9ceda630beb5c8b
Bablu <bablu.chilukoti@gmail.com>
2024-02-21 16:34:20 +0530

    roboshop-v3

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -1,6 +1,6 @@
 module "ec2" {
 for_each = var.components
-source = "../module"
+source = "./module"
 ami           = var.ami
 instance_type = each.value.instance_type
 vpc_security_group_ids = var.security_group
