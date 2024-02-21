0c1efcd465635d850ab06976d8003bf569803f20
Bablu <bablu.chilukoti@gmail.com>
2024-02-21 20:22:54 +0530

    roboshop-v3

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -12,6 +12,6 @@ resource "aws_route53_record" "DNS-records" {
  name    = "${lookup(aws_instance.instances, "Name", null)}.sreddy.online"
  type    = "A"
  ttl     = 30
- records = {lookup(aws_instance.instances, "private_ip", null)}
+ records = lookup(aws_instance.instances, "private_ip", null)
 }
 
