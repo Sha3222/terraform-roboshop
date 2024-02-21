4ef4068200627bfe25b8ac9ba18a4328a9812777
Bablu <bablu.chilukoti@gmail.com>
2024-02-21 17:36:25 +0530

    roboshop-v3

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -1,9 +1,11 @@
 module "ec2" {
+
 for_each = var.components
+
 source = "./module"
 ami           = var.ami
 instance_type = each.value.instance_type
-vpc_security_group_ids = var.security_group
+vpc_security_group_ids = var.security_groups
 zone_id = var.zone
 name = each.value.name
 }
--- roboshop-v3/variables.tf
+++ roboshop-v3/variables.tf
@@ -1,7 +1,7 @@
 variable "ami" {
 default = "ami-0f3c7d07486cad139"
 }
-variable "security_group" {
+variable "security_groups" {
 default = [ "sg-0738a0b37e3e0d683" ]
 }
 
@@ -11,14 +11,14 @@ default = "Z082591214E2K79TAHGZM"
 
 variable "components" {
 default = {
-frontend = {
-name = "frontend", instance_type = "t2.micro"
+ frontend = {
+  name = "frontend", instance_type = "t2.micro"
+ }
+ mongodb = {
+ name = "mongodb", instance_type = "t2.micro"
 }
-mongodb = {
-name = "mongodb", instance_type = "t2.micro"
-}
-cart = {
-name = "cart", instance_type = "t2.micro"
+ cart = {
+ name = "cart", instance_type = "t2.micro"
 }
 }
 }
