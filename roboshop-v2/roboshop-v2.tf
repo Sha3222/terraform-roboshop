3322c80c8e19f0aea98aeaf3097ad84e0cec292a
Bablu <bablu.chilukoti@gmail.com>
2024-02-21 20:47:01 +0530

    roboshop-v2

--- roboshop-v2/roboshop-v2.tf
+++ roboshop-v2/roboshop-v2.tf
@@ -47,5 +47,6 @@ resource "aws_route53_record" "DNS-records" {
  name    = "${lookup(each.value, "name", null)}.sreddy.online"
  type    = "A"
  ttl     = 30
- records = [lookup(lookup(aws_instance.instances, each.key, null), "private_ip", null) ]
+ //records = [lookup(lookup(aws_instance.instances, each.key, null), "private_ip", null) ]
+ records  = [aws.instance.instances.private_ip]
 }
