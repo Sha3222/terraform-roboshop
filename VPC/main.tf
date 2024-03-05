f484726d0455dafe080cb50ea52a3f7aeafadde1
Bablu <bablu.chilukoti@gmail.com>
2024-03-05 10:38:52 +0530

    vpc

---
+++ VPC/main.tf
@@ -0,0 +1,29 @@
+#Creating the VPC
+resource "aws_vpc" "vpc-1" {
+ cidr_block = "10.0.0.0/16"
+ tags = {
+ Name = "flipkart"
+ }
+}
+#Creating and attaching the internet gate way to flipkart-vpc
+
+resource "aws_internet_gateway" "flipkart-gateway" {
+ vpc_id = aws_vpc.vpc-1.id
+ tags = {
+ Name = Flipkart-gateway
+ }
+}
+resource "aws_internet_gateway_attachment"  "gate-way" {
+ internet_gateway_id = aws_internet_gateway.flipkart-gateway.id
+ vpc_id = aws_vpc.vpc-1.id
+}
+
+output "Result-vpc" {
+ value = aws_vpc.vpc-1
+}
+output "Result-gateway" {
+ value = aws_internet_gateway.flipkart-gateway
+}
+output "Result-vpc" {
+ value = aws_internet_gateway_attachment.gate-way
+}
