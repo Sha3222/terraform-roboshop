23213bcebd0598e26f8fec8e17c6b90bb2e541f3
Bablu <bablu.chilukoti@gmail.com>
2024-03-10 00:52:14 +0530

    vpc

--- VPC/main.tf
+++ VPC/main.tf
@@ -5,25 +5,17 @@ resource "aws_vpc" "vpc-1" {
  Name = "flipkart"
  }
 }
-#Creating and attaching the internet gate way to flipkart-vpc
+#Creating and attaching the internet flipkar-gate way to vpc-1
 
 resource "aws_internet_gateway" "flipkart-gateway" {
- vpc_id = aws_vpc.vpc-1.id
+ vpc_id = "${aws_vpc.vpc-1.id}"
  tags = {
  Name = "flipkart-gateway"
  }
 }
 resource "aws_internet_gateway_attachment"  "gate-way" {
- internet_gateway_id = aws_internet_gateway.flipkart-gateway.id
- vpc_id = aws_vpc.vpc-1.id
+ internet_gateway_id = "${aws_internet_gateway.flipkart-gateway.id}"
+ vpc_id = "${aws_vpc.vpc-1.id}"
 }
 
-output "Result-vpc" {
- value = aws_vpc.vpc-1
-}
-output "Result-gateway" {
- value = aws_internet_gateway.flipkart-gateway
-}
-output "Result-attach" {
- value = aws_internet_gateway_attachment.gate-way
-}
+#create a subnets
